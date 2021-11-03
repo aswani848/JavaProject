2aea780bf38e68e8c9b7a7cbe750746b59699a86
aswani848 <aswani.yaramala@gmail.com>
2021-11-03 13:02:17 -0600

    My First Java Progarm

--- $boxprogram.java
+++ $boxprogram.java
@@ -3,11 +3,11 @@ package $$$;
 public class $boxprogram {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=4;i++)
+		for(int i=1;i<=5;i++)
 		{
-			for(int j=1;j<=4;j++)
+			for(int j=1;j<=5;j++)
 			{
-				if(i==1 || i==4 || j==1 || j==4)
+				if(i==1 || i==5 || j==1 || j==5)
 				System.out.print('$'+ " ");
 				else
 				System.out.print(" " + " ");	
